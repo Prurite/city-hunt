7393e8e9769699dead637a9345f2be2d3e83b325
syeako <syeako@outlook.com>
2023-11-29 00:11:25 +0800

    Reimplement the alert system using database

--- src/pageCheckpoints.js
+++ src/pageCheckpoints.js
@@ -10,30 +10,41 @@ import handleApiError from "./AxiosError";
 const config = require("./config.json");
 const socket = io({ path: config.api_path + "/socket.io", transports: ['websocket'] });
 
-function CheckpointDetails (props) {
-  function f(text, def) { // strong when not default
+// Render the details (score, description, images) of a checkpoint
+function CheckpointDetails ({ point, show_images }) {
+  // Strong the text when it's not the default value
+  function f(text, def) {
     return text === def ? text
       : "<strong>" + text + "</strong>";
   }
-  const point = props.point;
+
   const dScores = [6, 5, 4, 3]; // default scores
+
+  // Display the scores
   let desc = f(point.scores[0], dScores[0]);
   for (let i = 1; i < point.scores.length; i++)
     desc = desc + "/" + f(point.scores[i], dScores[i]);
+
+  // Display the description
+  desc = desc + " " + point.desc;
+
+  // Render the images
   const images = point.images.map((value, index) => {
     return (
       <Image fluid key={value} src={config.static_image_path + "/" + value} />
     )
   })
-  desc = desc + " " + point.desc;
+
   return (<div className="checkpointDetails box">
     <p>{Parser(desc)}</p>
-    {props.images ? images : null}
+    {show_images ? images : null}
   </div>);
 }
 
-function CheckpointState (props) {
-  const state = props.point.state;
+// Render the state of a checkpoint
+function CheckpointState ({ point }) {
+
+  const state = point.state;
   if (state === "pending")
     return <span className="checkpointState"><i className="fa fa-spinner" /> 待审核</span>;
   else if (state === "accepted")
@@ -44,7 +55,11 @@ function CheckpointState (props) {
     return <span className="checkpointState"><i className="fa fa-bars" /> 未打卡</span>
 }
 
+// Render a full checkpoint, including header(name & state), images toggler,
+// checkpoint details, current status on the checkpoint and photo uploader
+// Props: point, onErr, fetchAlerts
 class Checkpoint extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -71,7 +86,14 @@ class Checkpoint extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.setState({ disabled: true });
+
+    // Check the attached photo
     const photo = this.state.selectedFile;
+    if (!photo) {
+      this.props.onErr("请选择一张图片");
+      this.setState({ disabled: false });
+      return;
+    }
     console.log(photo);
     var ext = photo.name.match(/\.([^\.]+)$/)[1];
     if (ext != "jpg" && ext != "jpeg") {
@@ -84,6 +106,8 @@ class Checkpoint extends React.Component {
       this.setState({ disabled: false });
       return;
     }
+
+    // Post to API
     const data = new FormData();
     data.append("photo", photo);
     data.append("checkpointid", this.props.point.id);
@@ -92,7 +116,7 @@ class Checkpoint extends React.Component {
       .then(res => {
         console.log(res);
         if (res.status == "200")
-          this.props.addAlert("打卡点 " + this.props.point.id + " 的打卡图片上传成功");
+          this.props.fetchAlerts();
         this.setState({ disabled: false });
       })
       .catch(err => {
@@ -102,6 +126,7 @@ class Checkpoint extends React.Component {
   }
 
   render() {
+    // Prepare the elements
     const point = this.props.point;
     const imagesButtonText = this.state.images
       ? "收起图片" : "展开图片";
@@ -116,9 +141,10 @@ class Checkpoint extends React.Component {
       : null;
     let myState = null;
     if (point.state === "accepted")
-      myState = <p> <strong>我的得分</strong> {point.score} </p>
+      myState = <p> <strong>我的得分</strong> {point.score} </p>;
     else if (point.state === "denied")
-      myState = <p> <strong>我的不通过原因</strong> {point.fail_reason} </p>
+      myState = <p> <strong>我的不通过原因</strong> {point.fail_reason} </p>;
+
     return (<Accordion.Item eventKey={"P" + point.id}>
       <Accordion.Header><div style={{width: "100%", marginRight: "10px"}}>
         {point.id} {point.name} <CheckpointState point={point} />
@@ -133,61 +159,68 @@ class Checkpoint extends React.Component {
             : null
           }
         </p>
-        <CheckpointDetails point={point} images={this.state.images} />
+        <CheckpointDetails point={point} show_images={this.state.images} />
         <p />
         <p> <strong>当前通过人数</strong> {point.passed} </p>
         <p>
           <strong>我的打卡图片</strong> {photoAction}
           {this.state.photo ? <Image fluid src={config.upload_image_path + "/" + point.photo} /> : null}
         </p>
-        {point.state != "accepted" &&
-        <Form onChange={this.handleInputChange} onSubmit={this.handleSubmit}> <InputGroup>
-          <Form.Control type="file" accept="image/*" />
-          <Button disabled={this.state.disabled} variant="primary" id={"B" + point.id} type="submit">
-            {point.state ? "重新上传" : "上传"}
-          </Button>
-        </InputGroup> </Form>}
+        { point.state != "accepted" &&
+          <Form onChange={this.handleInputChange} onSubmit={this.handleSubmit}> <InputGroup>
+            <Form.Control type="file" accept="image/*" />
+            <Button disabled={this.state.disabled} variant="primary" id={"B" + point.id} type="submit">
+              {point.state ? "重新上传" : "上传"}
+            </Button>
+          </InputGroup> </Form>
+        }
         <p />
         {myTime}
         {myState}
       </Accordion.Body>
-    </Accordion.Item>)
+    </Accordion.Item>);
   }
 }
 
-function CheckpointGroup ({ group, onErr, addAlert }) {
+// Render a group of checkpoints
+function CheckpointGroup ({ group, onErr, fetchAlerts }) {
   const checkpoints = group.points.map((value, index) => {
-    return <Checkpoint key={value.id} point={value} onErr={onErr} addAlert={addAlert} />;
+    return <Checkpoint key={value.id} point={value} onErr={onErr} fetchAlerts={fetchAlerts} />;
   })
   return (<Accordion.Item eventKey={"G" + group.id}>
     <Accordion.Header>{group.id} {group.name}</Accordion.Header>
     <Accordion.Body>
       <p>{Parser(group.desc)}</p>
-      <Accordion>
+      <Accordion alwaysOpen>
         {checkpoints}
       </Accordion>
     </Accordion.Body>
   </Accordion.Item>);
 }
 
-function CheckpointList ({ list, onErr, addAlert }) {
+// Render the checkpoint list consisting of groups
+function CheckpointList ({ list, onErr, fetchAlerts }) {
   const checkpointGroups = list.map((value, index) => {
-    return <CheckpointGroup key={value.id} group={value} onErr={onErr} addAlert={addAlert} />;
+    return <CheckpointGroup key={value.id} group={value} onErr={onErr} fetchAlerts={fetchAlerts} />;
   })
-  return <Accordion>{checkpointGroups}</Accordion>;
+  return <Accordion alwaysOpen> {checkpointGroups} </Accordion>;
 }
 
-function AlertList(props) {
+// Render the alerts list
+// The alerts is an array containing the direct output of the API
+function AlertsList({ alerts, removeAlert }) {
   return (<>
-    { props.alerts.map((value) => {
+    { alerts.map((value, index) => {
       return <Alert key={value} variant="primary" dismissible
-        onClose={() => { props.removeAlert(value) }}>
-          {value}
+        onClose={() => { removeAlert(value) }}>
+          {value.time} {value.content}
         </Alert>
     }) }
   </>)
 }
 
+// The page component
+// props: userId
 export default class PageCheckpoints extends React.Component {
   audio = new Audio(process.env.PUBLIC_URL + "/sound.wav");
   siren = new Audio(process.env.PUBLIC_URL + "/siren.wav");
@@ -208,10 +241,22 @@ export default class PageCheckpoints extends React.Component {
       })
   }
 
-  addAlert = (alert) => {
-    this.state.alerts = [alert].concat(this.state.alerts);
-    this.audio.play();
-    window.scrollTo(0, 0);
+  fetchAlerts = () => {
+    axios.get(config.api_path + "/alerts")
+      .then((res) => {
+        // When the response is different than current alerts,
+        // and the difference is not caused by deleting an alert,
+        // play a sound and scroll to top
+        if (JSON.stringify(this.state.alerts) !== JSON.stringify(res.data)
+          && this.state.alerts.length < res.data.length) {
+          this.audio.play();
+          window.scrollTo(0, 0);
+        }
+        this.setState({ alerts: res.data });
+      })
+      .catch((err) => {
+        this.setState({ err: { ...this.state.err, apiErr: handleApiError(err) } });
+      });
   }
 
   setErr = (err) => {
@@ -219,45 +264,12 @@ export default class PageCheckpoints extends React.Component {
   }
 
   componentDidMount() {
-    function stateText(state) {
-      if (!state)
-        return "未打卡";
-      const text = {
-        pending: "等待中",
-        accepted: "已通过",
-        denied: "未通过"
-      };
-      return text[state];
-    }
+    this.fetchAlerts();
     socket.on("update", (update) => {
       console.log("Receive update " + update);
-      setTimeout(() =>
-        axios.get(config.api_path + "/checkpoint/" + update)
-          .then((res) => {
-            const newPoint = res.data;
-            let newList = this.state.list.slice();
-            let newAlert = null;
-            for (let i = 0; i < newList.length; i++)
-              for (let j = 0; j < newList[i].points.length; j++)
-                if (newList[i].points[j].id === newPoint.id) {
-                  if (newList[i].points[j].state !== newPoint.state)
-                    newAlert = "打卡点 " + newPoint.id + " 的状态已从 " +
-                      stateText(newList[i].points[j].state) +
-                      " 变为 " + stateText(newPoint.state);
-                  newList[i].points[j] = newPoint;
-                }
-            this.setState({
-              list: newList,
-            });
-            if (newAlert) {
-              this.addAlert(newAlert);
-            }
-          })
-          .catch((err) => {
-            this.setState({ err: { ...this.state.err, apiErr: handleApiError(err) } });
-          }),
-        200);
+      this.fetchAlerts();
     });
+
     socket.on("disconnect", (err) => {
       console.log("Socket disconnected");
       this.setState({ err: {
@@ -283,8 +295,13 @@ export default class PageCheckpoints extends React.Component {
   }
 
   removeAlert(alert) {
-    const newAlerts = this.state.alerts.filter((x) => (x != alert));
-    this.setState({alerts: newAlerts});
+    axios.post(config.api_path + "/alert/delete", alert)
+      .then(() => {
+        this.fetchAlerts();
+      })
+      .catch((err) => {
+        this.setState({ err: { ...this.state.err, apiErr: handleApiError(err) } });
+      });
   }
 
   renderErr() {
@@ -298,8 +315,8 @@ export default class PageCheckpoints extends React.Component {
   render() {
     return (<div className="m-3">
       {this.renderErr()}
-      <AlertList alerts={this.state.alerts} removeAlert={(x) => this.removeAlert(x)} />
-      <CheckpointList list={this.state.list} onErr={this.setErr} addAlert={this.addAlert} />
+      <AlertsList alerts={this.state.alerts} removeAlert={(x) => this.removeAlert(x)} />
+      <CheckpointList list={this.state.list} onErr={this.setErr} fetchAlerts={this.fetchAlerts} />
     </div>)
   }
 }
